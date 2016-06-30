21f6676a87e07c102fed1a5045c1d784ee67d30d
Andrea <andry.tino@gmail.com>
2016-06-30 20:17:37 +0200

    Creating solution

---
+++ IPH/IPH/Iph.cs
@@ -0,0 +1,16 @@
+﻿/// <summary>
+/// Iph.cs
+/// Andrea Tino - 2016
+/// </summary>
+
+namespace IPH
+{
+    using System;
+
+    /// <summary>
+    /// Library entry point.
+    /// </summary>
+    public class Iph
+    {
+    }
+}
